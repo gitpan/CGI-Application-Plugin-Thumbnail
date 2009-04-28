fca48492874a46192fa84dbfa67984c97bd45783
Leo Charre <leocharre@cpan.org>
2009-04-28 16:24:17 +0000

    Import of LEOCHARRE/CGI-Application-Plugin-Thumbnail-1.05 from CPAN.
    gitpan-cpan-distribution: CGI-Application-Plugin-Thumbnail
    gitpan-cpan-version:      1.05
    gitpan-cpan-path:         LEOCHARRE/CGI-Application-Plugin-Thumbnail-1.05.tar.gz
    gitpan-cpan-author:       LEOCHARRE
    gitpan-cpan-maturity:     released

--- lib/CGI/Application/Plugin/Thumbnail.pm
+++ lib/CGI/Application/Plugin/Thumbnail.pm
@@ -6,7 +6,7 @@ use warnings;
 use Cwd;
 use Exporter;
 use vars qw($VERSION @ISA @EXPORT_OK %EXPORT_TAGS);
-our $VERSION = sprintf "%d.%02d", q$Revision: 1.3 $ =~ /(\d+)/g;
+our $VERSION = sprintf "%d.%02d", q$Revision: 1.5 $ =~ /(\d+)/g;
 @ISA = qw/ Exporter /;
 
 @EXPORT_OK = (qw(
@@ -107,7 +107,7 @@ sub _abs_thumbnail {
 
    require Image::Thumbnail;
 
-   my $thumb = new Image::Thumbnail(
+   my $thumb = Image::Thumbnail->new(
       size        => $size,
       input       => $abs_input,
       outputpath  => $abs_thumb,
@@ -131,31 +131,6 @@ sub __assure_thumb_dir {
    }   
    return $abs_loc;
 }
-
-#sub __create_a_thumbnail_object {
-#   my($self,$abs_thumb)
-#   
-# # THEN MAKE THE THUMB !!! .. uhm.. potentially...
-#   require Image::Magick;
-#   require Image::Magick::Thumbnail;
-#   
-#   my $img = new Image::Magick;
-#   $img->Read( $self->_img->abs_path );
-#
-#   # how does it compare to the restriction request??
-#   my $restriction = $self->thumbnail_restriction;
-#   $restriction=~/(\d+)x(\d+)/ or die;
-#   my ($h,$w) = ($1,$2);
-#   my ($W,$H) = $img->Get('width','height');
-#   if( $h >= $H and $w >= $W){
-#      # IMAGE IS SMALLER THEN THUMB
-#      debug(" h$h >= H$H or w$w >= W$W, image is smaller then thumb request\n");
-#      return $self->_img->abs_path;   
-#   }  
-#      
-#   my ($thumb,$x,$y) = Image::Magick::Thumbnail::create($img,$self->thumbnail_restriction);
-#   return $thumb;   
-#}
 
 
 sub __thumbnail_style {
@@ -233,201 +208,4 @@ sub thumbnail_header_add {
 
 
 1;
-
-__END__
-
-
-=pod
-
-=head1 NAME
-
-CGI::Application::Plugin::Thumbnail - have a runmode that makes thumbnails
-
-=head1 DESCRIPTION
-
-These methods allow your cgi app to have a runmmode that makes a thumbnail
-
-=head1 SYNOPSIS
-
-   use CGI::Application::Plugin::Thumbnail ':all';
-   use CGI::Application::Plugin::Stream 'stream_file';
-
-
-   sub thumbnail : Runmode {
-	   my $self = shift;
-
-      # was an original image requested, and did it exist on disk?
-      $self->get_abs_image('rel_path') or return;
-         
-      # get the corresponding abs pathto the thumbnail, create if not exists
-      $self->abs_thumbnail or return;
-      
-      # stream the thumbnail image
-      $self->stream_file( $self->abs_thumbnail ) 
-         or warn("cant stream");
-         
-      return;
-   }
-
-=head1 METHODS
-
-
-=head2 get_abs_image()
-
-optional argument is what query string param will hold the relative path to the image we want to make a thumb for.
-if none specified, uses 'rel_path'
-returns boolean, if it found something or not.
-calling this method is optional
-
-   $self->get_abs_image('rel_path');   
-
-=head2 set_abs_image()
-
-optional argument is absolute path to original image
-if none provided, will resolve with get_rel_path()
-calling this method is optional
-
-   $self->set_abs_image('/home/myself/images/image1.jpg');   
-
-=head2 abs_image()
-
-returns absolute path to image we want to make a thumb of
-if you set via set_abs_image() or specified query param via get_abs_image() , will mirror this.
-you will likely call this method in your thumbnail runmode
-
-   $self->abs_image or warn("you did not ask for an image or that image does not exist");
-   
-=head2 abs_thumbnail()
-
-returns absolute path to thumbnail
-if it did not exist, we would try to make it, if we could not make it, returns undef and warns
-this is the minimum method you need to call, to stream the image
-
-   $self->abs_thumbnail or warn("no thumbnail was made or no image requested");
-
-
-=head1 CHANGING THUMBNAIL BEFORE SAVING
-
-What if you want to provide your own funky style to the thumbnail before saving??
-
-then override __thumbnail_style()
-
-Example:
-
-
-   use CGI::Application::Plugin::Thumbnail;
-   use CGI::Application::Plugin::Stream;
-
-
-   sub __thumbnail_style {
-      my ($self,$thumb) = @_;
-      
-      $thumb->Quantize( colorspace => 'gray' ); 
-		$thumb->Set( compression => '8' );
-      return 1;   
-   }
-
-
-
-
-   sub thumbnail : Runmode {
-	   my $self = shift;
-      $self->get_abs_image('rel_path') or return;   
-      
-      $self->abs_thumbnail or return;  
-   
-      $self->stream_file( $self->abs_thumbnail ) 
-         or warn("thumbnail runmode: could not stream thumb ".$self->abs_thumbnail);
-      return;
-   }
-
-=head2 __thumbnail_style()
-
-Does nothing. You can override this to change your thumbnail
-it is called when a thumbnail is creted, 
-*after* the original is resized, but *before* it is saved.
-receives $thumb image magick object.
-You do not need to return the object. See L<CHANGING THUMBNAIL BEFORE SAVING>
-
-=cut
-
-=head1 PARAMS TO CONSTRUCTOR
-
-   new My::Cgiapp(
-      PARAMS => {       
-         thumbnail_rel_dir => '.thumbnails',                  
-         thumbnail_restriction => '100x100',
-      },   
-   );
-
-
-You do not have to provide parameters. Defaults are provided.
-
-=head2 WHAT PICTURE?
-
-What you tell the runmode thumbnail is not what thumbnail you want, but what original picture you want a thumbnail *of*.
-
-The way you tell it is via the query string, if you want a thumbnail of /home/me/public_html/img/one.jpg, then the query 
-string would read one of these:
-
-   ?rm=thumbnail&rel_path=img/one.jpg
-   ?rm=thumbnail&rel_path=/img/one.jpg
-
-=head2 THUMBNAIL DOCROOT
-
-You can define where the thumbnails are stored via parameter to constructor.
-We attempt to create if not there.
-
-   new My::Cgiapp(
-      PARAMS => { 
-         thumbnail_rel_dir => '/.thumbnails' },
-   );
-
-Default is shown above.
-
-=head2 THUMBNAIL RESTRICTION, DIMENSIONS
-
-We define dimensions as maximum width and maximum height.
-If you do not define dimensions, default is 100x100.
-You may also defined dimmensions via the query string:
-
-   ?rm=thumbnail&rel_path=img/one.jpg&thumbnail_restriction=100x100
-
-We store thumbs by dimension for example these are requests and their destinations
-Keep in mind that if the thumbnail dimensions are larger then the image, no thumbnail is made, 
-the original is streamed back.
-
-   ?rm=thumbnail&rel_path=img/one.jpg
-   DOCUMENT_ROOT/.thumbnails/100x100/img/one.jpg
-   
-   ?rm=thumbnail&rel_path=img/one.jpg&thumbnail_restriction=100x100
-   DOCUMENT_ROOT/.thumbnails/100x100/img/one.jpg
-
-   ?rm=thumbnail&rel_path=img/one.jpg&thumbnail_restriction=600x600
-   DOCUMENT_ROOT/.thumbnails/600x600/img/one.jpg
-
-=head1 CHANGES
-
-Since version 1.03, instead of using Image::Magick::Thumbnail, we use Image::Thumbnail.
-
-Requested by Lyle.
-
-=head1 REVISION
-
-$Revision: 1.3 $
-
-=head1 CAVEATS & BUGS
-
-Still a work in progress.
-If you want any changes in this module, please contact the AUTHOR.
-
-   
-=head1 AUTHOR
-
-Leo Charre leocharre at cpan dot org.
-
-=cut
-
-
-
 
--- t/1.t
+++ t/1.t
@@ -1,16 +1,40 @@
 use Test::Simple 'no_plan';
+
 use strict;
 use lib './lib';
 use lib './t';
 use Cwd;
 
+
 $ENV{CGI_APP_RETURN_ONLY} = 1;
-
 $CGI::Application::Plugin::Thumbnail::DEBUG = 1;
-
 use PTest;
 
-ok(1);
+
+BEGIN {
+   ok(1);
+
+   my @pms; # which work ?
+   for my $pm ( qw/GD Image::Magick Imager BoguZNonseze::Module::herew2123523::indeed/ ){
+      
+      my $r ;
+      $r = eval("require $pm;");
+      $r||=0;
+      
+      warn("  - require $pm: $r\n");
+      $r or next;
+      push @pms, $pm;   
+   }
+   warn "WORKING MODULES: @pms\n\n";
+   scalar @pms or warn("You dont have GD, Image::Magick or Imager installed, skipping.") and exit;
+
+}
+
+
+
+
+
+
 
 
 $ENV{DOCUMENT_ROOT} = cwd().'/t';
@@ -23,7 +47,7 @@ ok(   $p->set_abs_image(cwd().'/t/ayn_rand.jpg'),'set_abs_image()');
 
 #print STDERR  $p->_img->abs_path."\n";
 
-ok( $p->thumbnail_header_add, 'thumbnai lheader add');
+ok( $p->thumbnail_header_add, 'thumbnail header add');
 
 my $rd = $p->_thumbnail_rel_dir;
 ok($rd, " $rd");
